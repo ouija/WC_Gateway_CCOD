b5ae4259c8f6ee42e7f9a913ab86d8ac28044a33
ouija <mail@ouija.xyz>
2018-04-02 22:10:27 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WC_Gateway_CCOD
+WooCommerce Credit Card on Delivery
+
+Extends WooCommerce by adding a payment option of credit card on delivery.
---
+++ wc-gateway-ccod.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Credit Card on Delivery
+ * Plugin URI: https://github.com/ouija/WC_Gateway_CCOD
+ * Description: Extends WooCommerce by adding a payment option of credit card on delivery.
+ * Version: 1.0
+ * Requires at least: 4.0
+ * Tested up to: 4.9
+ * WC requires at least: 2.5
+ * WC tested up to: 3.3
+ * Author: ouija
+ * Author URI: http://ouija.xyz
+ * License: GPLv2 or later
+ * Text Domain: ccod
+ */
+
+/* Initalization */
+add_action( 'plugins_loaded', 'ccod_init', 0 );
+function ccod_init() {
+
+	/* Checks if WooCommerce is installed and activated */
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
+	
+	/* Inlcude payment gateway class */
+	include_once( 'class-wc-gateway-ccod.php' );
+
+	/* Adds ccod payment gateway */
+	add_filter( 'woocommerce_payment_gateways', 'ccod_add_gateway' );
+	function ccod_add_gateway( $methods ) {
+		$methods[] = 'WC_Gateway_CCOD';
+		return $methods;
+	}
+}
+
+/* Plugin action links */
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'ccod_action_links' );
+function ccod_action_links( $links ) {
+	$plugin_links = array(
+		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section=ccod' ) . '">' . __( 'Settings', 'ccod' ) . '</a>',
+	);
+	return array_merge( $plugin_links, $links );	
+}
